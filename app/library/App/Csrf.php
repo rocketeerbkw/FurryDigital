0afe2c97fabfde53489f72a788dc468f5d660d5d
Buster Silver <loobalightdark@gmail.com>
2016-02-10 18:04:01 -0600

    Add documentation and an exception wrapper to CSRF validation.

--- app/library/App/Csrf.php
+++ app/library/App/Csrf.php
@@ -8,15 +8,42 @@ class Csrf
      */
     protected $_session;
 
+    /**
+     * @var int The length of the string to generate.
+     */
     protected $_csrf_code_length = 10;
+
+    /**
+     * @var int The lifetime (in seconds) of an un-renewed CSRF token.
+     */
+    protected $_csrf_lifetime = 3600;
+
+    /**
+     * @var string The default namespace used for token generation.
+     */
+    protected $_csrf_default_namespace = 'general';
 
     public function __construct(Session $session)
     {
         $this->_session = $session->get('csrf');
     }
 
-    public function generate($namespace = 'general')
+    /**
+     * Generate a Cross-Site Request Forgery (CSRF) protection token.
+     * The "namespace" allows distinct CSRF tokens for different site functions,
+     * while not crowding the session namespace with one token for each action.
+     *
+     * If not renewed (with another "generate" call to the same namespace),
+     * a CSRF token will last the time specified in $this->_csrf_lifetime.
+     *
+     * @param string $namespace
+     * @return null|String
+     */
+    public function generate($namespace = null)
     {
+        if ($namespace === null)
+            $namespace = $this->_csrf_default_namespace;
+
         $key = NULL;
         if (isset($this->_session[$namespace]))
         {
@@ -36,8 +63,18 @@ class Csrf
         return $key;
     }
 
-    public function verify($key, $namespace = 'general')
+    /**
+     * Verify a supplied CSRF token against the tokens stored in the session.
+     *
+     * @param $key
+     * @param string $namespace
+     * @return array
+     */
+    public function verify($key, $namespace = null)
     {
+        if ($namespace === null)
+            $namespace = $this->_csrf_default_namespace;
+
         if (empty($key))
             return array('is_valid' => false, 'message' => 'A CSRF token is required for this request.');
 
@@ -53,12 +90,30 @@ class Csrf
             return array('is_valid' => false, 'message' => 'Invalid CSRF token supplied.');
 
         // Compare against time threshold (CSRF keys last 60 minutes).
-        $threshold =  $namespace_info['timestamp']+60*60;
+        $threshold =  $namespace_info['timestamp']+$this->_csrf_lifetime;
 
         if (time() >= $threshold)
             return array('is_valid' => false, 'message' => 'This CSRF token has expired!');
 
         return array('is_valid' => true);
+    }
+
+    /**
+     * Wrapper to the verify() function that triggers an exception for invalid tokens.
+     *
+     * @param $key
+     * @param null $namespace
+     * @return bool
+     * @throws Exception
+     */
+    public function requireValid($key, $namespace = null)
+    {
+        $verify_result = $this->verify($key, $namespace);
+
+        if ($verify_result['is_valid'])
+            return true;
+        else
+            throw new Exception('Cannot validate CSRF token: '.$verify_result['message']);
     }
 
     /**
